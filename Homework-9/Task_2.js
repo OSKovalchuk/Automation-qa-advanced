3a8aada030c953c9813920ff21ad00a75e6d7814
OSKovalchuk <Olga.Kovalchuk@symphony-solutions.com>
2025-10-29 20:25:58 +0200

    Add Homework-9

---
+++ Homework-9/Task_2.js
@@ -0,0 +1,13 @@
+
+const book = {
+  title: "JavaScript for Kids",
+  author: "Nick Morgan",
+  year: 2014
+};
+
+
+const { title, author } = book;
+
+
+console.log("Book name:", title);
+console.log("Author:", author);
---
+++ Homework-9/Task_4.js
@@ -0,0 +1,15 @@
+
+let person = {
+  firstName: "Olena",
+  lastName: "Koval",
+  age: 25
+};
+
+
+person.email = "olena.koval@example.com";
+
+
+delete person.age;
+
+
+console.log(person);
